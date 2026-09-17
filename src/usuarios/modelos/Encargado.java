6229e81f4059c7a2c43a62a6a32092eb76aaddd8
barti06 <bartipdl1@gmail.com>
2026-09-17 00:08:23 -0300

    nuevo paquete usuarios.modelos + nuevas clases Cliente Empleado y Encargado

---
+++ src/usuarios/modelos/Encargado.java
@@ -0,0 +1,16 @@
+package usuarios.modelos;
+
+/**
+ * @author barti
+ */
+public class Encargado {
+    public String correo;
+    public String clave;
+    public String apellido;
+    public String nombre;
+
+    public void mostrar() {
+        System.out.println("Apellido y Nombre: " + apellido + nombre +
+                           " | Correo: " + correo + " | Clave: " + clave);
+    }
+}
