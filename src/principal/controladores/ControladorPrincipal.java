24c26cd7f5942150b84e605d3141a2541a69b43f
ShailaHussein <shaila24hussein@gmail.com>
2026-09-18 18:37:14 -0300

    Se crearon los ArrayList para cada clase, se intanciaron cada una de las clases y se realizaron operaciones con los ArrayList como: Agregar elementos, recorrer y  modificar.

--- src/principal/controladores/ControladorPrincipal.java
+++ src/principal/controladores/ControladorPrincipal.java
@@ -4,7 +4,10 @@
  */
 package principal.controladores;
 
+import java.util.ArrayList;
+import usuarios.modelos.*;
 import productos.modelos.Producto;
+
 
 /**
  *
@@ -12,11 +15,83 @@ import productos.modelos.Producto;
  */
 public class ControladorPrincipal {
     public static void main(String[] args) {
-        Producto p1 = new Producto();
-        Producto p2 = new Producto();
-        p1.descripcion = "Hamburguesa";
-        p2.descripcion = "Pizza";
-        p1.mostrar();
-        p2.mostrar();
+          ArrayList<Cliente> listaClientes = new ArrayList<>();
+          ArrayList<Empleado> listaEmpleados = new ArrayList<>();
+          ArrayList<Encargado> listaEnacargados = new ArrayList<>();
+          ArrayList<Producto> listaProductos = new ArrayList<>();
+          
+          //Instanciamos objetos
+          Cliente cliente1 = new Cliente();
+          Cliente cliente2 = new Cliente();
+          Cliente cliente3 = new Cliente();
+          
+          Empleado empleado1 = new Empleado();
+          Empleado empleado2 = new Empleado();
+          Empleado empleado3 = new Empleado();
+          
+          Encargado encargado1 = new Encargado();
+          Encargado encargado2 = new Encargado();
+          Encargado encargado3 = new Encargado();
+          
+          Producto producto1 = new Producto();
+          Producto producto2 = new Producto();
+          Producto producto3 = new Producto();
+          
+          //Los agregamos a sus respectivas colecciones
+          listaEnacargados.add(encargado1);
+          listaEnacargados.add(encargado2);
+          listaEnacargados.add(encargado3);
+          
+          listaClientes.add(cliente1);
+          listaClientes.add(cliente2);
+          listaClientes.add(cliente3);
+          
+          listaEmpleados.add(empleado1);
+          listaEmpleados.add(empleado2);
+          listaEmpleados.add(empleado3);
+          
+          listaProductos.add(producto1);
+          listaProductos.add(producto2);
+          listaProductos.add(producto3);
+          
+          //Recorremos los arrayList
+//          for (Cliente unCliente : listaClientes) {
+//            unCliente.mostrar();
+//        }
+//          
+//        for (Empleado unEmpleado : listaEmpleados) {
+//            unEmpleado.mostrar();
+//        }
+//        
+//        for (Encargado unEncargado : listaEnacargados) {
+//            unEncargado.mostrar();
+//        }
+//          
+//        for (Producto unProducto : listaProductos) {
+//            unProducto.mostrar();
+//        }
+        
+        //Modificamos algunos objetos
+        producto1.codigo = 1;
+        producto1.descripcion = "Arroz integral";
+        producto1.estado = false;
+        producto1.precio = 1400;
+        
+        //Recorremos los arrayList
+          for (Cliente unCliente : listaClientes) {
+            unCliente.mostrar();
+        }
+          
+        for (Empleado unEmpleado : listaEmpleados) {
+            unEmpleado.mostrar();
+        }
+        
+        for (Encargado unEncargado : listaEnacargados) {
+            unEncargado.mostrar();
+        }
+          
+        for (Producto unProducto : listaProductos) {
+            unProducto.mostrar();
+        }
     }
 }
