3bf60ad2f8d194c923144b7aa4d85f2af0db59dc
Juan Manuel Guaraz Vargas <juanmanuelguarazvargas@gmail.com>
2026-09-22 18:25:25 -0300

    Agregado de metodos asignar y mostrar junto al contstructor de la clase Encargado

--- src/usuarios/modelos/Encargado.java
+++ src/usuarios/modelos/Encargado.java
@@ -5,9 +5,38 @@ public class Encargado {
     public  String clave;
     public String apellido;
     public String nombre;
+
+    public Encargado(String correo, String clave, String apellido, String nombre) {
+        this.correo = correo;
+        this.clave = clave;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
     
     public void mostrar(){
         System.out.printf("\n\tEncargado <%s %s>: \n||Correo: %s\n||Clave: %s\n", apellido, nombre, correo, clave);
+    }
+    
+    public void asignarNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String mostrarNombre(){
+        return this.nombre;
+    }
+    public String mostrarApellido(){
+        return this.apellido;
+    }
+    public void asignarClave(String clave){
+        this.clave = clave;
+    }
+    public String mostrarClave(){
+        return this.clave;
+    }
+    public void asignarCorreo(String correo){
+        this.correo = correo;
+    }
+    public String mostrarCorreo(){
+        return this.correo;
     }
     
 }
