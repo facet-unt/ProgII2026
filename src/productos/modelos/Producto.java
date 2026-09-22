e97c8fc17d8af58b32565e1674ffa85a12e1e806
barti06 <bartipdl1@gmail.com>
2026-09-22 17:54:06 -0300

    Encapsulamiento de Producto

--- src/productos/modelos/Producto.java
+++ src/productos/modelos/Producto.java
@@ -10,11 +10,14 @@ package productos.modelos;
  * @author luis
  */
 public class Producto {
-    public int codigo;
-    public String descripcion;
-    public String categoria;
-    public String estado;
-    public float precio;
+    public Producto(int codigo, String descripcion, String categoria, String estado,
+                    float precio) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.estado = estado;
+        this.precio = precio;
+    }
 
     public void mostrar() {
         System.out.println(descripcion);
@@ -24,5 +27,29 @@ public class Producto {
         System.out.println(codigo);
     }
 
+    // Getters
+    public int verCodigo() { return this.codigo; }
+    public String verDescripcion() { return this.descripcion; }
+    public String verCategoria() { return this.categoria; }
+    public String verEstado() { return this.estado; }
+    public float verPrecio() { return this.precio; }
+
+    // Setters
+    public void asignarCodigo(int nuevoCodigo) { this.codigo = nuevoCodigo; }
+    public void asignarDescripcion(String nuevaDescripcion) {
+        this.descripcion = nuevaDescripcion;
+    }
+    public void asignarCategoria(String nuevaCategoria) {
+        this.categoria = nuevaCategoria;
+    }
+    public void asignarEstado(String nuevoEstado) { this.estado = nuevoEstado; }
+    public void asignarPrecio(float nuevoPrecio) { this.precio = nuevoPrecio; }
+
     @Override public String toString() { return this.descripcion; }
+
+    private int codigo;
+    private String descripcion;
+    private String categoria;
+    private String estado;
+    private float precio;
 }
