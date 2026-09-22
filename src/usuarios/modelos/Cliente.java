b6a427742d0b173cb610000cf869b630feba7f3b
Juan Manuel Guaraz Vargas <juanmanuelguarazvargas@gmail.com>
2026-09-22 18:23:35 -0300

    Agregado de metodos asignar y mostrar junto al contstructor de la clase Cliente

--- src/usuarios/modelos/Cliente.java
+++ src/usuarios/modelos/Cliente.java
@@ -9,12 +9,40 @@ package usuarios.modelos;
  * @author estudiante
  */
 public class Cliente {
-    public String correo;
-    public String clave;
-    public String apellido;
-    public String nombre;
+    private String correo;
+    private String clave;
+    private String apellido;
+    private String nombre;
+
+    public Cliente(String correo, String clave, String apellido, String nombre) {
+        this.correo = correo;
+        this.clave = clave;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
     
     public void mostrar(){
         System.out.printf("\n\tCliente <%s %s>: \n||Correo: %s\n||Clave: %s\n", apellido, nombre, correo, clave);
+    }
+    public void asignarNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String mostrarNombre(){
+        return this.nombre;
+    }
+    public String mostrarApellido(){
+        return this.apellido;
+    }
+    public void asignarClave(String clave){
+        this.clave = clave;
+    }
+    public String mostrarClave(){
+        return this.clave;
+    }
+    public void asignarCorreo(String correo){
+        this.correo = correo;
+    }
+    public String mostrarCorreo(){
+        return this.correo;
     }
 }
