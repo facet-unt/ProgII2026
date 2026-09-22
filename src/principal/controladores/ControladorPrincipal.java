89c6b37f23dfe2bf3e5f110dbe016c4fbd49b38b
Maria de la Paz Castellano Rengel <mariadelapazcastellano@gmail.com>
2026-09-22 18:32:36 -0300

    Realizando cambios en la clase Producto (encapsulamiento, get/set y constructor)

--- src/principal/controladores/ControladorPrincipal.java
+++ src/principal/controladores/ControladorPrincipal.java
@@ -16,83 +16,81 @@ public class ControladorPrincipal {
     public static void main(String[] args) {
         
         
-        ArrayList<Cliente> clientes = new ArrayList ();
-        ArrayList<Encargado> encargados = new ArrayList ();
-        ArrayList<Empleado> empleados = new ArrayList ();
+//        ArrayList<Cliente> clientes = new ArrayList ();
+//        ArrayList<Encargado> encargados = new ArrayList ();
+//        ArrayList<Empleado> empleados = new ArrayList ();
         ArrayList<Producto> productos = new ArrayList ();
         
-         Cliente cliente1 = new Cliente();
-         Cliente cliente2 = new Cliente();
-         Cliente cliente3 = new Cliente();
-         
-         Encargado encargado1 = new Encargado();
-         Encargado encargado2 = new Encargado();
-         Encargado encargado3 = new Encargado();
-         
-         Empleado empleado1 = new Empleado();
-         Empleado empleado2 = new Empleado();
-         Empleado empleado3 = new Empleado();
+//         Cliente cliente1 = new Cliente();
+//         Cliente cliente2 = new Cliente();
+//         Cliente cliente3 = new Cliente();
+//         
+//         Encargado encargado1 = new Encargado();
+//         Encargado encargado2 = new Encargado();
+//         Encargado encargado3 = new Encargado();
+//         
+//         Empleado empleado1 = new Empleado();
+//         Empleado empleado2 = new Empleado();
+//         Empleado empleado3 = new Empleado();
+//        
+        
+        Producto hamburguesa = new Producto(1234,"Hamburguesa","Comida","Disponible",8000f);
+        Producto pizza = new Producto(5678,"Pizza","Comida","No disponible",12000f);
+        Producto papasFritas = new Producto(91011,"Papas Fritas","Comida","Disponible",5000f);
         
         
-        Producto hamburguesa = new Producto();
-        Producto pizza = new Producto();
-        Producto papasFritas = new Producto();
-        
-        hamburguesa.descripcion = "Hamburguesa";
-        pizza.descripcion = "Pizza";
-        papasFritas.descripcion = "Papas Fritas";
-        hamburguesa.precio = 7000f;
-        
-        cliente1.apellido = "Varga";
-        cliente2.apellido = "Molina";
-        cliente3.apellido = "Gomez";
-        
-        encargado1.apellido ="Fernandez";
-        encargado2.apellido ="Martinez";
-        encargado3.apellido ="Alvares";
-        
-        empleado1.apellido ="Fuentes";
-        empleado2.apellido ="Lopez";
-        empleado3.apellido ="Aguilera";
-        
+//        cliente1.apellido = "Varga";
+//        cliente2.apellido = "Molina";
+//        cliente3.apellido = "Gomez";
+//        
+//        encargado1.apellido ="Fernandez";
+//        encargado2.apellido ="Martinez";
+//        encargado3.apellido ="Alvares";
+//        
+//        empleado1.apellido ="Fuentes";
+//        empleado2.apellido ="Lopez";
+//        empleado3.apellido ="Aguilera";
+//        
         productos.add(hamburguesa);
         productos.add(pizza);
         productos.add(papasFritas);
         
-        clientes.add(cliente1);
-        clientes.add(cliente2);
-        clientes.add(cliente3);
+//        clientes.add(cliente1);
+//        clientes.add(cliente2);
+//        clientes.add(cliente3);
+//        
+//        encargados.add(encargado1);
+//        encargados.add(encargado2);
+//        encargados.add(encargado3);
+//        
+//        empleados.add(empleado1);
+//        empleados.add(empleado2);
+//        empleados.add(empleado3);
         
-        encargados.add(encargado1);
-        encargados.add(encargado2);
-        encargados.add(encargado3);
         
-        empleados.add(empleado1);
-        empleados.add(empleado2);
-        empleados.add(empleado3);
-        
-        
-        System.out.println("------productos------\n");
+        System.out.println("------Productos------\n");
         for (Producto p: productos ){
             p.mostrar();
         }
-        System.out.println("------clientes------\n");
-        for (Cliente c: clientes ){
-            c.mostrar();
-        }
-        System.out.println("------encargados------\n");
-        for (Encargado e: encargados ){
-            e.mostrar();
-        }
-        System.out.println("------empleados------\n");
-        for (Empleado e: empleados ){
-            e.mostrar();
-        }
+//        System.out.println("------clientes------\n");
+//        for (Cliente c: clientes ){
+//            c.mostrar();
+//        }
+//        System.out.println("------encargados------\n");
+//        for (Encargado e: encargados ){
+//            e.mostrar();
+//        }
+//        System.out.println("------empleados------\n");
+//        for (Empleado e: empleados ){
+//            e.mostrar();
+//        }
         
-        System.out.println("cambiando precio de hamburguesa\n");
-        
-        productos.get(0).precio=8000f;
-        
+        System.out.println("Cambiando...\n");
+//        
+        productos.get(0).asignarPrecio(12000f);
+        productos.get(1).asignarEstado("No disponible");
+        productos.get(2).asignarCodigo(4567);
+//        
         for (Producto p: productos ){
             p.mostrar();
         }
--- src/productos/modelos/Producto.java
+++ src/productos/modelos/Producto.java
@@ -9,15 +9,65 @@ package productos.modelos;
  * @author estudiante
  */
 public class Producto {
-    public int codigo;
-    public String descripcion;
-    public String categoria;
-    public String estado;
-    public float precio;
+    private int codigo;
+    private String descripcion;
+    private String categoria;
+    private String estado;
+    private float precio;
+
+    public int verCodigo() {
+        return codigo;
+    }
+
+    public String verDescripcion() {
+        return descripcion;
+    }
+
+    public String verCategoria() {
+        return categoria;
+    }
+
+    public String verEstado() {
+        return estado;
+    }
+
+    public float verPrecio() {
+        return precio;
+    }
+
+    public void asignarCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void asignarDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void asignarCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void asignarEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void asignarPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public Producto(int codigo, String descripcion, String categoria, String estado, float precio) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.estado = estado;
+        this.precio = precio;
+    }
+    
+    
     
     public void mostrar() {        
-        System.out.println("producto: "+descripcion +" codigo: " +codigo );
-        System.out.println("estado: "+estado +" | precio: " +precio+ "\n");
+        System.out.println("Producto: "+descripcion +" Codigo: " +codigo );
+        System.out.println("Estado: "+estado +" | Precio: " +precio+ "\n");
     }
     
     @Override
