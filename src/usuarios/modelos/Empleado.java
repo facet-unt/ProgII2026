cba956d49020fe413adcb2ea7c1537ed77991ccb
Juan Manuel Guaraz Vargas <juanmanuelguarazvargas@gmail.com>
2026-09-22 18:24:44 -0300

    Agregado de metodos asignar y mostrar junto al contstructor de la clase Empleado

--- src/usuarios/modelos/Empleado.java
+++ src/usuarios/modelos/Empleado.java
@@ -13,9 +13,38 @@ public class Empleado {
     public String clave;
     public String apellido;
     public String nombre;
+
+    public Empleado(String correo, String clave, String apellido, String nombre) {
+        this.correo = correo;
+        this.clave = clave;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
     
     public void mostrar(){
         System.out.printf("\n\tEmpleado <%s %s>: \n||Correo: %s\n||Clave: %s\n", apellido, nombre, correo, clave);
+    }
+    
+    public void asignarNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String mostrarNombre(){
+        return this.nombre;
+    }
+    public String mostrarApellido(){
+        return this.apellido;
+    }
+    public void asignarClave(String clave){
+        this.clave = clave;
+    }
+    public String mostrarClave(){
+        return this.clave;
+    }
+    public void asignarCorreo(String correo){
+        this.correo = correo;
+    }
+    public String mostrarCorreo(){
+        return this.correo;
     }
     
 }
